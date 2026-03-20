69cd21b780e4cb8b3ed4cf00bd98bf728ca43afa
Logan Harbour <loganhharbour@gmail.com>
2026-03-20 12:12:33 -0600

    Create base class for common transformation logic

--- include/transforms/OpenMCCellTransformBase.h
+++ include/transforms/OpenMCCellTransformBase.h
@@ -18,33 +18,47 @@
 
 #pragma once
 
-#include "CriticalitySearchBase.h"
-#include "OpenMCCellTransformBase.h"
-#include "CardinalEnums.h"
-#include "UserObjectInterface.h"
+#ifdef ENABLE_OPENMC_COUPLING
 
-/**
- * Perform a criticality search based on a rotation angle
- */
-class RotationSearch : public CriticalitySearchBase, public OpenMCCellTransformBase
+#include "MooseTypes.h"
+#include "InputParameters.h"
+
+class MooseEnum;
+
+class OpenMCCellAverageProblem;
+
+class OpenMCCellTransformBase
 {
 public:
   static InputParameters validParams();
 
-  RotationSearch(const InputParameters & parameters);
+  /// Enumeration for the transform type
+  static const MooseEnum transform_type;
+  /// The symbols for each dimension in the transform vector: ["φ", "θ", "ψ"]
+  static const std::array<std::string, 3> transform_vector_symbols;
+  /// The symbols for the transform vector in a list form: "φ, θ, ψ"
+  static const std::string transform_vector_symbols_list;
 
-  /** Verifies that the definition of the OpenMCCellTransform
-   * UserObject is valid for a rotational criticality search
+  OpenMCCellTransformBase(const MooseObject & moose_object);
+
+  /**
+   * Do a transformation on the given cells.
+   *
+   * @param transform_type The transform type (translation or rotation)
+   * @param transform_vector The vector by to transform
    */
-  void checkValidVectorValueForRotationSearch();
-
-  virtual void updateOpenMCModel(const Real & angle) override;
+  void transform(const MooseEnum & transform_type, const Point & transform_vector);
 
 protected:
-  virtual std::string units() const override { return "[degrees]"; }
-  // TODO: logan doesn't know what to put here
-  virtual std::string quantity() const override { return "fixme"; }
+  /// OpenMC cell IDs to which the translation will be applied
+  const std::set<int32_t> _cell_ids;
 
-  /// the index of the rotational axis used to search for criticality
-  const int _rotation_axis_idx;
+private:
+  /// The MooseObject owning this transform
+  const MooseObject & _moose_object;
+
+  /// The OpenMC problem; for calling the transforms
+  OpenMCCellAverageProblem & _openmc_problem;
 };
+
+#endif
